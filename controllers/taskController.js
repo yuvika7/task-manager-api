bfba34aeadad37db3e2d804ad6080d766a2d6bbb
Yuvika <yuvikagilhotra725@gmail.com>
2026-05-11 23:43:49 +0530

    Completed full task CRUD APIs with authorization

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -23,6 +23,100 @@ const createTask = async (req, res) => {
   }
 };
 
+const getTasks = async (req, res) => {
+
+  try {
+
+    const tasks = await Task.find({
+      user: req.user.id,
+    });
+
+    res.status(200).json(tasks);
+
+  } catch (error) {
+
+    res.status(500).json({
+      message: error.message,
+    });
+
+  }
+};
+
+const updateTask = async (req, res) => {
+
+  try {
+
+    const task = await Task.findById(req.params.id);
+
+    if (!task) {
+      return res.status(404).json({
+        message: "Task not found",
+      });
+    }
+
+    // Ownership validation
+    if (task.user.toString() !== req.user.id) {
+      return res.status(401).json({
+        message: "Not authorized",
+      });
+    }
+
+    const updatedTask = await Task.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+      }
+    );
+
+    res.status(200).json(updatedTask);
+
+  } catch (error) {
+
+    res.status(500).json({
+      message: error.message,
+    });
+
+  }
+};
+
+const deleteTask = async (req, res) => {
+
+  try {
+
+    const task = await Task.findById(req.params.id);
+
+    if (!task) {
+      return res.status(404).json({
+        message: "Task not found",
+      });
+    }
+
+    // Ownership validation
+    if (task.user.toString() !== req.user.id) {
+      return res.status(401).json({
+        message: "Not authorized",
+      });
+    }
+
+    await task.deleteOne();
+
+    res.status(200).json({
+      message: "Task deleted successfully",
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      message: error.message,
+    });
+
+  }
+};
+
 module.exports = {
   createTask,
+  getTasks,
+  updateTask,
+  deleteTask,
 };
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -6,8 +6,15 @@ const protect = require("../middleware/authMiddleware");
 
 const {
   createTask,
+    getTasks,
+    updateTask,
+      deleteTask,
+
 } = require("../controllers/taskController");
 
 router.post("/", protect, createTask);
+router.get("/", protect, getTasks);
+router.put("/:id", protect, updateTask);
+router.delete("/:id", protect, deleteTask);
 
 module.exports = router;
